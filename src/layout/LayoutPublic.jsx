42e5a13e86fb66d6e555aa805fe35f24738d88a4
juancmdev <juancmorenoj@gmail.com>
2025-02-02 15:14:40 -0500

    Update LayoutPublic.jsx

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const LayoutPublic = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
+      <NavBar />
       <main className="container">
-        <NavBar />
+        {navigation.state === "loading" && (
+          <div className="alert alert-info my-5">Loading...</div>
+        )}
         <Outlet />
         {/* Se trae el elemento del archivo de configuración, el element (Home, Blog, About)*/}
       </main>
